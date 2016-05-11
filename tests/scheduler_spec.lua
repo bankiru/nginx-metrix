58797afa5510a9fe6de5a13653acb0544bceeab0
Boris Gorbylev <ekho@ekho.name>
2016-05-11 09:44:28 +0300

    aggregation on timer (alpha 4)

--- tests/scheduler_spec.lua
+++ tests/scheduler_spec.lua
@@ -1,12 +1,34 @@
 require('tests.bootstrap')(assert)
 
 describe('scheduler', function()
+    local scheduler
+    local match = require 'luassert.match'
+
+    before_each(function()
+        scheduler = require 'nginx-metrix.scheduler'
+    end)
+
+    after_each(function()
+        package.loaded['nginx-metrix.collectors'] = nil
+        _G.ngx = nil
+    end)
+
     it('attach_collector', function()
-        pending('Not implemented yet')
+        scheduler.attach_collector({})
+        assert.is_equal(0, length(scheduler.__private__.get_collectors()))
+
+        scheduler.attach_collector({name='test', periodically=function() end})
+        assert.is_equal(1, length(scheduler.__private__.get_collectors()))
     end)
 
     it('start', function()
-        pending('Not implemented yet')
+        _G.ngx = mock({timer = mock({at = function() end}), log = function() end})
+
+        stub.new(_G.ngx.timer, 'at').on_call_with(match._,match._,match._).returns(true, nil)
+
+        scheduler.start()
+        assert.spy(_G.ngx.timer.at).was.called(1)
+        assert.spy(_G.ngx.log).was_not.called()
     end)
 
     it('handler', function()

27c4fd29b110a726e2e6b41c432974ab4b862020
Boris Gorbylev <ekho@ekho.name>
2016-05-16 11:27:00 +0300

    Removed nginx-conf and vagrant. Closes #1 and part of #5

--- rsync.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-for i in {1..59}; do
-    rsync -qa --delete --inplace -T /tmp /vagrant/nginx-metrix/* /etc/nginx/nginx-metrix
-    rsync -qa --delete --inplace -T /tmp /vagrant/nginx-conf/* /etc/nginx/nginx-metrix-conf
-    sleep 1
-done

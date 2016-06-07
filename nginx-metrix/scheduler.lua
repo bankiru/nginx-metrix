100c8d7eabc7ac97d8f7b6c9f166c585c50d9b87
Boris Gorbylev <ekho@ekho.name>
2016-06-07 16:01:11 +0300

    fixed aggregation process locking (#33)

--- nginx-metrix/scheduler.lua
+++ nginx-metrix/scheduler.lua
@@ -12,57 +12,62 @@ local attach_collector = function(collector)
   end
 end
 
-local lock_key = '--aggregator-lock--'
-local lock_timeout = 2
-
-local get_lock = function()
-  local lock_id = ngx.now()
-  local success, _, _ = dict.add(lock_key, lock_id, lock_timeout)
-  if success then
-    return true, lock_id
-  end
-  return false, nil
-end
-
-local drop_lock = function(lock_id)
-  if lock_id == dict.get(lock_key) then
-    dict.delete(lock_key)
-    return true
-  end
-  return false
-end
-
 local delay = 1
 
-local handler
-handler = function(premature, collectors_list)
-  if premature then
+local lock_key = '--scheduler-lock--'
+local lock_timeout = delay * 0.95
+
+local setup_lock = function()
+  return (dict.add(lock_key, lock_key, lock_timeout))
+end
+
+local process = function(collectors_list, worker_id)
+  if length(collectors_list) == 0 then
+    logger.debug(("[scheduler #%s] collectors list is empty, skipping"):format(worker_id))
     return
   end
 
-  if length(collectors_list) > 0 then
-    local lock_success, lock_id = get_lock()
-    if lock_success then
-      iter(collectors_list):map(
-        function(collector)
-          return collector, ngx.thread.spawn(function() collector:aggregate() end)
-        end
-      ):each(
-        function(collector, thread)
-          local ok, res = ngx.thread.wait(thread)
-          if not ok then
-            logger.error(("failed to run Collector<%s>:aggregate(): "):format(collector.name), res)
-          end
-        end
-      )
-
-      drop_lock(lock_id)
-    end
+  if not setup_lock() then
+    logger.debug(("[scheduler #%s] lock still exists, skipping"):format(worker_id))
+    return
   end
 
-  local ok, err = ngx.timer.at(delay, handler, collectors_list)
+  logger.debug(("[scheduler #%s] lock obtained, aggregating"):format(worker_id))
+  iter(collectors_list):map(
+    function(collector)
+      logger.debug(("[scheduler #%s] spawned thread for Collector<%s>"):format(worker_id, collector.name))
+      return collector, ngx.thread.spawn(function()
+        logger.debug(("[scheduler #%s] thread processing started for Collector<%s>"):format(worker_id, collector.name))
+        collector:aggregate()
+        logger.debug(("[scheduler #%s] thread processing finished for Collector<%s>"):format(worker_id, collector.name))
+      end)
+    end
+  ):each(
+    function(collector, thread)
+      local ok, res = ngx.thread.wait(thread)
+      if not ok then
+        logger.error(("[scheduler #%s] failed to run Collector<%s>:aggregate()"):format(worker_id, collector.name), res)
+      else
+        logger.debug(("[scheduler #%s] thread finished for Collector<%s>"):format(worker_id, collector.name))
+      end
+    end
+  )
+end
+
+local handler
+handler = function(premature, collectors_list, worker_id)
+  if premature then
+    logger.debug(("[scheduler #%s] exited by premature flag"):format(worker_id))
+    return
+  end
+
+  local ok, err
+
+  process(collectors_list, worker_id)
+
+  ok, err = ngx.timer.at(delay, handler, collectors_list, worker_id)
   if not ok then
-    logger.error("Failed to continue the scheduler - failed to create the timer: ", err)
+    logger.error(("[scheduler #%s] Failed to continue the scheduler - failed to create the timer"):format(worker_id), err)
     return
   end
 end
@@ -70,11 +75,21 @@ end
 ---
 -- @return bool
 local start = function()
-  local ok, err = ngx.timer.at(delay, handler, collectors)
-  if not ok then
-    logger.error("Failed to start the scheduler - failed to create the timer: ", err)
+  local worker_id, incr_err = dict.safe_incr('worker_id')
+  if incr_err ~= nil then
+    logger.error('Can not make worker_id', incr_err)
     return false
   end
+
+  logger.debug(('[scheduler #%s] starting'):format(worker_id))
+
+  local ok, err = ngx.timer.at(delay, handler, collectors, worker_id)
+  if not ok then
+    logger.error(("[scheduler #%s] Failed to start the scheduler - failed to create the timer"):format(worker_id), err)
+    return false
+  end
+
+  logger.debug(('[scheduler #%s] started'):format(worker_id))
   return true
 end
 
@@ -87,13 +102,14 @@ exports.start = start
 
 if __TEST__ then
   exports.__private__ = {
-    get_collectors = function() return collectors end,
-    lock_key       = function(value) if value == nil then return lock_key else lock_key = value end end,
-    lock_timeout   = function(value) if value == nil then return lock_timeout else lock_timeout = value end end,
-
-    handler        = handler,
-    get_lock       = get_lock,
-    drop_lock      = drop_lock,
+    collectors = function() return collectors end,
+    delay = function(value) if value == nil then return delay else delay = value end end,
+    lock_key = function(value) if value == nil then return lock_key else lock_key = value end end,
+    lock_timeout = function(value) if value == nil then return lock_timeout else lock_timeout = value end end,
+    process = function(...) return process(...) end,
+    handler = handler,
+    setup_lock = setup_lock,
+    _process = function(body) process = body end,
   }
 end
 
--- tests/scheduler_spec.lua
+++ tests/scheduler_spec.lua
@@ -47,101 +47,74 @@ describe('scheduler', function()
     mock.revert(_G.ngx)
   end)
 
-
-  it('get lock ok', function()
-    local test_lock_id = 'test_lock_id'
-    stub.new(_G.ngx, 'now').on_call_with().returns(test_lock_id)
-
+  it('setup lock ok', function()
     dict_mock.add.on_call_with(scheduler.__private__.lock_key(), match._, scheduler.__private__.lock_timeout()).returns(true, nil, false)
-    local lock_success, lock_id = scheduler.__private__.get_lock()
+    local lock_success = scheduler.__private__.setup_lock()
     assert.spy(dict_mock.add).was.called(1)
     assert.spy(dict_mock.add).was.called_with(scheduler.__private__.lock_key(), match._, scheduler.__private__.lock_timeout())
     assert.is_true(lock_success)
-    assert.is_equal(test_lock_id, lock_id)
   end)
 
-  it('get lock failed on existent lock', function()
-    local test_lock_id = 'test_lock_id'
-    stub.new(_G.ngx, 'now').on_call_with().returns(test_lock_id)
-
+  it('setup lock failed on existent lock', function()
     dict_mock.add.on_call_with(scheduler.__private__.lock_key(), match._, scheduler.__private__.lock_timeout()).returns(false, 'exists', false)
-    local lock_success, lock_id = scheduler.__private__.get_lock()
+    local lock_success = scheduler.__private__.setup_lock()
     assert.spy(dict_mock.add).was.called(1)
     assert.spy(dict_mock.add).was.called_with(scheduler.__private__.lock_key(), match._, scheduler.__private__.lock_timeout())
     assert.is_false(lock_success)
-    assert.is_nil(lock_id)
   end)
 
-  it('get lock ok on stale lock', function()
-    local test_lock_id = 'test_lock_id'
-    stub.new(_G.ngx, 'now').on_call_with().returns(test_lock_id)
-
+  it('setup lock ok on stale lock', function()
     scheduler.__private__.lock_timeout(0.1)
 
-    local lock_success, lock_id
+    local lock_success
 
     dict_mock.add.on_call_with(scheduler.__private__.lock_key(), match._, match._).returns(true, nil, false)
-    lock_success, lock_id = scheduler.__private__.get_lock()
+    lock_success = scheduler.__private__.setup_lock()
     assert.is_true(lock_success)
-    assert.is_equal(test_lock_id, lock_id)
     assert.spy(dict_mock.add).was.called_with(scheduler.__private__.lock_key(), match._, scheduler.__private__.lock_timeout())
 
-    test_lock_id = 'yet-another-test-lock-id'
-    stub.new(_G.ngx, 'now').on_call_with().returns(test_lock_id)
-
     dict_mock.add.on_call_with(scheduler.__private__.lock_key(), match._, match._).returns(true, nil, true)
-    lock_success, lock_id = scheduler.__private__.get_lock()
+    lock_success = scheduler.__private__.setup_lock()
     assert.is_true(lock_success)
-    assert.is_equal(test_lock_id, lock_id)
     assert.spy(dict_mock.add).was.called_with(scheduler.__private__.lock_key(), match._, scheduler.__private__.lock_timeout())
 
     assert.spy(dict_mock.add).was.called(2)
   end)
 
-  it('drop lock ok on own lock', function()
-    local test_lock_id = 'test_lock_id'
-
-    dict_mock.get.on_call_with(scheduler.__private__.lock_key()).returns(test_lock_id, 0)
-
-    assert.is_true(scheduler.__private__.drop_lock(test_lock_id))
-
-    assert.spy(dict_mock.get).was.called_with(scheduler.__private__.lock_key())
-    assert.spy(dict_mock.get).was.called(1)
-    assert.spy(dict_mock.delete).was.called_with(scheduler.__private__.lock_key())
-    assert.spy(dict_mock.delete).was.called(1)
-  end)
-
-  it('drop lock failed on non own lock', function()
-    dict_mock.get.on_call_with(scheduler.__private__.lock_key()).returns(123.456, 0)
-
-    assert.is_false(scheduler.__private__.drop_lock(456.123))
-
-    assert.spy(dict_mock.get).was.called_with(scheduler.__private__.lock_key())
-    assert.spy(dict_mock.get).was.called(1)
-    assert.spy(dict_mock.delete).was_not.called()
-  end)
-
-
   it('attach_collector', function()
     scheduler.attach_collector({})
-    assert.is_equal(0, length(scheduler.__private__.get_collectors()))
+    assert.is_equal(0, length(scheduler.__private__.collectors()))
 
     scheduler.attach_collector({ name = 'test', aggregate = function() end })
-    assert.is_equal(1, length(scheduler.__private__.get_collectors()))
+    assert.is_equal(1, length(scheduler.__private__.collectors()))
   end)
 
-  it('start failed', function()
-    stub.new(_G.ngx.timer, 'at').on_call_with(1, match.is_function(), {}).returns(false, 'test error')
+  it('start failed because worker_id', function()
+    local worker_id = 13
+    dict_mock.safe_incr.on_call_with('worker_id').returns(nil, 'test error')
 
     assert.is_false(scheduler.start())
-    assert.spy(_G.ngx.timer.at).was.called_with(1, match.is_function(), {})
+    assert.spy(_G.ngx.timer.at).was_not.called()
+    assert.spy(logger.error).was.called_with('Can not make worker_id', 'test error')
+    assert.spy(logger.error).was.called(1)
+  end)
+
+  it('start failed because timer', function()
+    local worker_id = 13
+    dict_mock.safe_incr.on_call_with('worker_id').returns(worker_id, nil)
+    stub.new(_G.ngx.timer, 'at').on_call_with(1, match.is_function(), {}, worker_id).returns(false, 'test error')
+
+    assert.is_false(scheduler.start())
+    assert.spy(_G.ngx.timer.at).was.called_with(1, match.is_function(), {}, worker_id)
     assert.spy(_G.ngx.timer.at).was.called(1)
-    assert.spy(logger.error).was.called_with('Failed to start the scheduler - failed to create the timer: ', 'test error')
+    assert.spy(logger.error).was.called_with('[scheduler #' .. worker_id .. '] Failed to start the scheduler - failed to create the timer', 'test error')
     assert.spy(logger.error).was.called(1)
   end)
 
   it('start without collectors', function()
-    stub.new(_G.ngx.timer, 'at').on_call_with(1, match.is_function(), {}).returns(true, nil)
+    local worker_id = 13
+    dict_mock.safe_incr.on_call_with('worker_id').returns(worker_id, nil)
+    stub.new(_G.ngx.timer, 'at').on_call_with(1, match.is_function(), {}, worker_id).returns(true, nil)
 
     assert.is_true(scheduler.start())
     assert.spy(_G.ngx.timer.at).was.called(1)
@@ -152,138 +125,114 @@ describe('scheduler', function()
     local test_collector = { name = 'test', aggregate = function() end }
     scheduler.attach_collector(test_collector)
 
-    stub.new(_G.ngx.timer, 'at').on_call_with(1, match.is_function(), { test_collector }).returns(true, nil)
+    local worker_id = 13
+    dict_mock.safe_incr.on_call_with('worker_id').returns(worker_id, nil)
+    stub.new(_G.ngx.timer, 'at').on_call_with(1, match.is_function(), { test_collector }, worker_id).returns(true, nil)
 
     assert.is_true(scheduler.start())
-    assert.spy(_G.ngx.timer.at).was.called_with(1, match.is_function(), { test_collector })
+    assert.spy(_G.ngx.timer.at).was.called_with(1, match.is_function(), { test_collector }, worker_id)
     assert.spy(_G.ngx.timer.at).was.called(1)
     assert.spy(logger.error).was_not.called()
   end)
 
   it('handler on premature call', function()
-    local test_collector = mock({ name = 'test', aggregate = function() end })
-    scheduler.__private__.handler(true, { test_collector })
-    assert.spy(_G.ngx.thread.spawn).was_not.called()
-    assert.spy(_G.ngx.thread.wait).was_not.called()
+    local process_stub = mock({process = function() end}).process
+    scheduler.__private__._process(process_stub)
+
+    scheduler.__private__.handler(true, {}, 1)
+    assert.spy(process_stub).was_not.called()
     assert.spy(_G.ngx.timer.at).was_not.called()
     assert.spy(logger.error).was_not.called()
   end)
 
   it('handler failed to start next iter', function()
-    local test_collector = mock({ name = 'test', aggregate = function() end })
+    local worker_id = 13
 
-    local test_lock_id = 'test_lock_id'
-    stub.new(_G.ngx, 'now').on_call_with().returns(test_lock_id)
-    dict_mock.add.on_call_with(scheduler.__private__.lock_key(), match._, match._).returns(true, nil, false)
-    dict_mock.get.on_call_with(scheduler.__private__.lock_key()).returns(test_lock_id, 0)
+    local process_stub = mock({process = function() end}).process
+    scheduler.__private__._process(process_stub)
 
-    local thread = { [[thread stub]] }
-    stub.new(_G.ngx.thread, 'spawn').on_call_with(match.is_function()).returns(thread)
-    stub.new(_G.ngx.thread, 'wait').on_call_with(thread).returns(true, nil)
+    stub.new(_G.ngx.timer, 'at').on_call_with(1, match.is_function(), {}, worker_id).returns(false, 'test error')
 
-    stub.new(_G.ngx.timer, 'at').on_call_with(1, match.is_function(), {test_collector}).returns(false, 'test error')
+    scheduler.__private__.handler(false, {}, worker_id)
 
-    scheduler.__private__.handler(false, {test_collector})
+    assert.spy(process_stub).was.called_with({}, worker_id)
+    assert.spy(process_stub).was.called(1)
 
-    assert.spy(_G.ngx.thread.spawn).was.called(1)
-    assert.spy(_G.ngx.thread.wait).was.called(1)
-    assert.spy(_G.ngx.timer.at).was.called(1)
-
-    assert.spy(dict_mock.add).was.called_with(scheduler.__private__.lock_key(), test_lock_id, scheduler.__private__.lock_timeout())
-    assert.spy(dict_mock.add).was.called(1)
-    assert.spy(dict_mock.delete).was.called_with(scheduler.__private__.lock_key())
-    assert.spy(dict_mock.delete).was.called(1)
-
-    assert.spy(logger.error).was.called_with('Failed to continue the scheduler - failed to create the timer: ', 'test error')
+    assert.spy(logger.error).was.called_with('[scheduler #' .. worker_id .. '] Failed to continue the scheduler - failed to create the timer', 'test error')
     assert.spy(logger.error).was.called(1)
   end)
 
-  it('handler without collectors', function()
-    local test_lock_id = 'test_lock_id'
+  it('process without collectors', function()
+    local worker_id = 13
 
-    stub.new(_G.ngx.timer, 'at').on_call_with(1, match.is_function(), {}).returns(true, nil)
+    scheduler.__private__.process({}, worker_id)
 
-    scheduler.__private__.handler(false, {})
-
-    assert.spy(_G.ngx.thread.spawn).was_not.called()
-    assert.spy(_G.ngx.thread.wait).was_not.called()
-    assert.spy(_G.ngx.timer.at).was.called_with(1, match.is_function(), {})
-    assert.spy(_G.ngx.timer.at).was.called(1)
-    assert.spy(logger.error).was_not.called()
     assert.spy(dict_mock.add).was_not.called()
-    assert.spy(dict_mock.get).was_not.called()
-    assert.spy(dict_mock.delete).was_not.called()
+    assert.spy(logger.error).was_not.called()
+    assert.spy(logger.debug).was.called_with('[scheduler #' .. worker_id .. '] collectors list is empty, skipping')
   end)
 
-  it('handler log error if can not wait thread', function()
+  it('process skips if can not setup lock', function()
+    local worker_id = 13
+
+    dict_mock.add.on_call_with(scheduler.__private__.lock_key(), scheduler.__private__.lock_key(), scheduler.__private__.lock_timeout()).returns(false, 'exists', false)
+
+    scheduler.__private__.process({[[collector]]}, worker_id)
+
+    assert.spy(dict_mock.add).was.called_with(scheduler.__private__.lock_key(), scheduler.__private__.lock_key(), scheduler.__private__.lock_timeout())
+    assert.spy(dict_mock.add).was.called(1)
+    assert.spy(logger.error).was_not.called()
+    assert.spy(logger.debug).was.called_with('[scheduler #' .. worker_id .. '] lock still exists, skipping')
+  end)
+
+  it('process logs error if can not wait thread', function()
     local test_collector = mock({ name = 'test', aggregate = function() end })
 
-    local test_lock_id = 'test_lock_id'
-    stub.new(_G.ngx, 'now').on_call_with().returns(test_lock_id)
+    local worker_id = 13
+
     dict_mock.add.on_call_with(scheduler.__private__.lock_key(), match._, match._).returns(true, nil, false)
-    dict_mock.get.on_call_with(scheduler.__private__.lock_key()).returns(test_lock_id, 0)
 
     local thread = { [[thread stub]] }
 
     stub.new(_G.ngx.thread, 'spawn').on_call_with(match.is_function()).returns(thread)
     stub.new(_G.ngx.thread, 'wait').on_call_with(thread).returns(false, 'test error')
-    stub.new(_G.ngx.timer, 'at').on_call_with(1, match.is_function(), { test_collector }).returns(true, nil)
+    stub.new(_G.ngx.timer, 'at').on_call_with(1, match.is_function(), { test_collector }, worker_id).returns(true, nil)
 
-    scheduler.__private__.handler(false, { test_collector })
+    scheduler.__private__.process({ test_collector }, worker_id)
+
+    assert.spy(dict_mock.add).was.called_with(scheduler.__private__.lock_key(), scheduler.__private__.lock_key(), scheduler.__private__.lock_timeout())
+    assert.spy(dict_mock.add).was.called(1)
 
     assert.spy(_G.ngx.thread.spawn).was.called_with(match.is_function())
     assert.spy(_G.ngx.thread.wait).was.called_with(thread)
-    assert.spy(logger.error).was.called_with('failed to run Collector<test>:aggregate(): ', 'test error')
+
+    assert.spy(logger.error).was.called_with('[scheduler #' .. worker_id .. '] failed to run Collector<test>:aggregate()', 'test error')
     assert.spy(logger.error).was.called(1)
-    assert.spy(_G.ngx.timer.at).was.called(1)
-    assert.spy(dict_mock.add).was.called_with(scheduler.__private__.lock_key(), test_lock_id, scheduler.__private__.lock_timeout())
-    assert.spy(dict_mock.add).was.called(1)
-    assert.spy(dict_mock.delete).was.called_with(scheduler.__private__.lock_key())
-    assert.spy(dict_mock.delete).was.called(1)
   end)
 
-  it('handler with collectors', function()
+  it('process with collectors', function()
     local test_collector = mock({ name = 'test', aggregate = function() end })
 
-    local test_lock_id = 'test_lock_id'
-    stub.new(_G.ngx, 'now').on_call_with().returns(test_lock_id)
-    dict_mock.add.on_call_with(scheduler.__private__.lock_key(), match._, match._).returns(true, nil, false)
-    dict_mock.get.on_call_with(scheduler.__private__.lock_key()).returns(test_lock_id, 0)
+    local worker_id = 13
 
-    local thread = { [[thread stub]] }
-
-    stub.new(_G.ngx.thread, 'spawn').on_call_with(match.is_function()).returns(thread)
-    stub.new(_G.ngx.thread, 'wait').on_call_with(thread).returns(true, nil)
-    stub.new(_G.ngx.timer, 'at').on_call_with(1, match.is_function(), { test_collector }).returns(true, nil)
     dict_mock.add.on_call_with(scheduler.__private__.lock_key(), match._, match._).returns(true, nil, false)
 
-    scheduler.__private__.handler(false, { test_collector })
+    local thread = { func = nil }
+
+    stub.new(_G.ngx.thread, 'spawn').on_call_with(match.is_function()).invokes(function(func) thread.func = func; return thread end)
+    stub.new(_G.ngx.thread, 'wait').on_call_with(match._).invokes(function(thread) thread.func(); return true, nil end)
+
+    logger.error.on_call_with(match._, match._).invokes(function(...) print(require 'inspect'({...})) end)
+
+    scheduler.__private__.process({ test_collector }, worker_id)
+
+    assert.spy(dict_mock.add).was.called_with(scheduler.__private__.lock_key(), scheduler.__private__.lock_key(), scheduler.__private__.lock_timeout())
+    assert.spy(dict_mock.add).was.called(1)
 
     assert.spy(_G.ngx.thread.spawn).was.called_with(match.is_function())
     assert.spy(_G.ngx.thread.wait).was.called_with(thread)
-    assert.spy(_G.ngx.timer.at).was.called_with(1, match.is_function(), { test_collector })
-    assert.spy(_G.ngx.timer.at).was.called(1)
+    assert.spy(test_collector.aggregate).was.called(1)
+
     assert.spy(logger.error).was_not.called()
-    assert.spy(dict_mock.add).was.called_with(scheduler.__private__.lock_key(), test_lock_id, scheduler.__private__.lock_timeout())
-    assert.spy(dict_mock.add).was.called(1)
-    assert.spy(dict_mock.delete).was.called_with(scheduler.__private__.lock_key())
-    assert.spy(dict_mock.delete).was.called(1)
-  end)
-
-  it('only one handler per time', function()
-    local test_collector = mock({ name = 'test', aggregate = function() end })
-
-    dict_mock.add.on_call_with(scheduler.__private__.lock_key(), match._, match._).returns(false, 'exists', false)
-
-    stub.new(_G.ngx.timer, 'at').on_call_with(1, match.is_function(), { test_collector }).returns(true, nil)
-
-    scheduler.__private__.handler(false, { test_collector })
-    assert.spy(_G.ngx.thread.spawn).was_not.called()
-    assert.spy(_G.ngx.thread.wait).was_not.called()
-    assert.spy(_G.ngx.timer.at).was.called_with(1, match.is_function(), { test_collector })
-    assert.spy(_G.ngx.timer.at).was.called(1)
-    assert.spy(logger.error).was_not.called()
-    assert.spy(dict_mock.get).was_not.called(1)
-    assert.spy(dict_mock.delete).was_not.called(1)
   end)
 end)
